88574d7db2035db55f006798280269fdd55fb17a
aktov <aktovi@gmail.com>
2022-12-23 11:16:11 +0700

    Initial commit

---
+++ src/MainWindow.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import javax.swing.*;
+
+public class MainWindow extends JFrame {
+
+    public MainWindow() {
+        setTitle("Змейка");
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setSize(320, 345);
+        setLocation(400, 400);
+        setVisible(true);
+        add(new GameField());
+    }
+
+    public static void main(String[] args) {
+        MainWindow mw = new MainWindow();
+    }
+}
